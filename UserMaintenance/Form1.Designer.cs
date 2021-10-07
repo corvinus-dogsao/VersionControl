73011ad59e0c97d8bdbd0e380cd5ac6a3bca06b5
Örs Csák <ors.csak@stud.uni-corvinus.hu>
2021-10-07 16:52:01 +0200

    commit changes

--- UserMaintenance/Form1.Designer.cs
+++ UserMaintenance/Form1.Designer.cs
@@ -1,5 +1,4 @@
-﻿
-namespace UserMaintenance
+﻿amespace UserMaintenance
 {
     partial class Form1
     {
@@ -29,13 +28,93 @@ namespace UserMaintenance
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.listBoxUsers = new System.Windows.Forms.ListBox();
+            this.textBoxFullName = new System.Windows.Forms.TextBox();
+            this.labelFullName = new System.Windows.Forms.Label();
+            this.buttonAdd = new System.Windows.Forms.Button();
+            this.buttonWriteOut = new System.Windows.Forms.Button();
+            this.buttonDelete = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // listBoxUsers
+            // 
+            this.listBoxUsers.FormattingEnabled = true;
+            this.listBoxUsers.Location = new System.Drawing.Point(13, 13);
+            this.listBoxUsers.Name = "listBoxUsers";
+            this.listBoxUsers.Size = new System.Drawing.Size(232, 407);
+            this.listBoxUsers.TabIndex = 0;
+            // 
+            // textBoxFullName
+            // 
+            this.textBoxFullName.Location = new System.Drawing.Point(318, 28);
+            this.textBoxFullName.Name = "textBoxFullName";
+            this.textBoxFullName.Size = new System.Drawing.Size(139, 20);
+            this.textBoxFullName.TabIndex = 2;
+            // 
+            // labelFullName
+            // 
+            this.labelFullName.AutoSize = true;
+            this.labelFullName.Location = new System.Drawing.Point(266, 31);
+            this.labelFullName.Name = "labelFullName";
+            this.labelFullName.Size = new System.Drawing.Size(35, 13);
+            this.labelFullName.TabIndex = 4;
+            this.labelFullName.Text = "label2";
+            // 
+            // buttonAdd
+            // 
+            this.buttonAdd.Location = new System.Drawing.Point(470, 22);
+            this.buttonAdd.Name = "buttonAdd";
+            this.buttonAdd.Size = new System.Drawing.Size(129, 30);
+            this.buttonAdd.TabIndex = 5;
+            this.buttonAdd.Text = "button1";
+            this.buttonAdd.UseVisualStyleBackColor = true;
+            this.buttonAdd.Click += new System.EventHandler(this.buttonAdd_Click);
+            // 
+            // buttonWriteOut
+            // 
+            this.buttonWriteOut.Location = new System.Drawing.Point(388, 390);
+            this.buttonWriteOut.Name = "buttonWriteOut";
+            this.buttonWriteOut.Size = new System.Drawing.Size(129, 30);
+            this.buttonWriteOut.TabIndex = 6;
+            this.buttonWriteOut.Text = "button1";
+            this.buttonWriteOut.UseVisualStyleBackColor = true;
+            this.buttonWriteOut.Click += new System.EventHandler(this.buttonWriteOut_Click);
+            // 
+            // buttonDelete
+            // 
+            this.buttonDelete.Location = new System.Drawing.Point(253, 390);
+            this.buttonDelete.Name = "buttonDelete";
+            this.buttonDelete.Size = new System.Drawing.Size(129, 30);
+            this.buttonDelete.TabIndex = 7;
+            this.buttonDelete.Text = "button1";
+            this.buttonDelete.UseVisualStyleBackColor = true;
+            this.buttonDelete.Click += new System.EventHandler(this.buttonDelete_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(611, 438);
+            this.Controls.Add(this.buttonDelete);
+            this.Controls.Add(this.buttonWriteOut);
+            this.Controls.Add(this.buttonAdd);
+            this.Controls.Add(this.labelFullName);
+            this.Controls.Add(this.textBoxFullName);
+            this.Controls.Add(this.listBoxUsers);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.ListBox listBoxUsers;
+        private System.Windows.Forms.TextBox textBoxFullName;
+        private System.Windows.Forms.Label labelFullName;
+        private System.Windows.Forms.Button buttonAdd;
+        private System.Windows.Forms.Button buttonWriteOut;
+        private System.Windows.Forms.Button buttonDelete;
     }
 }
-
--- UserMaintenance/Form1.cs
+++ UserMaintenance/Form1.cs
@@ -3,18 +3,77 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using UserMaintenance.Entities;
 
 namespace UserMaintenance
 {
     public partial class Form1 : Form
     {
+        BindingList<User> users = new BindingList<User>();
         public Form1()
         {
             InitializeComponent();
+            labelFullName.Text = Resource1.FullName;
+            buttonAdd.Text = Resource1.Add;
+            buttonWriteOut.Text = Resource1.WriteOut;
+            buttonDelete.Text = Resource1.Delete;
+
+            listBoxUsers.DataSource = users;
+            listBoxUsers.DisplayMember = "FullName";
+            listBoxUsers.ValueMember = "ID";
+        }
+
+        private void buttonAdd_Click(object sender, EventArgs e)
+        {
+            var u = new User()
+            {
+                FullName = textBoxFullName.Text
+            };
+            users.Add(u);
+            textBoxFullName.Clear();
+        }
+
+        private void buttonWriteOut_Click(object sender, EventArgs e)
+        {
+            //StreamWriter sw;
+            SaveFileDialog save = new SaveFileDialog();
+            save.Filter = "txt files (*.txt)|*.txt|All files (*.*)|*.*";
+            save.FilterIndex = 2;
+            save.DefaultExt = "txt";
+            try
+            {
+                if (save.ShowDialog() == DialogResult.OK)
+                {
+                    using (StreamWriter sw = new StreamWriter(save.FileName))
+                    {
+                        foreach (var item in users)
+                        {
+                            sw.Write("ID: ");
+                            sw.Write(item.ID);
+                            sw.Write(" Teljes név: ");
+                            sw.Write(item.FullName);
+                            sw.WriteLine();
+                        }
+                    }
+                    MessageBox.Show("Sikeres fájlbaírás!");
+                }
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void buttonDelete_Click(object sender, EventArgs e)
+        {
+            var selected = (User)listBoxUsers.SelectedItem;
+            users.Remove(selected);
         }
     }
 }
