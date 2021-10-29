9f07f5f236f2d287543937d8e648181c5285061e
Örs Csák <ors.csak@stud.uni-corvinus.hu>
2021-10-29 10:46:19 +0200

    LoadXml

--- week6/Form1.cs
+++ week6/Form1.cs
@@ -7,11 +7,13 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Xml;
 
 namespace week6
 {
     public partial class Form1 : Form
     {
+        BindingList<RateData> Rates = new BindingList<RateData>();
         public Form1()
         {
             InitializeComponent();
@@ -30,6 +32,11 @@ namespace week6
             series.BorderWidth = 2;
             var chartArea = chartRateData.ChartAreas[0];
             chartArea.AxisX.MajorGrid.Enabled = false;
+        }
+
+        private void LoadXml (string input)
+        {
+            XmlDocument xml = new XmlDocument();
         }
 
       
