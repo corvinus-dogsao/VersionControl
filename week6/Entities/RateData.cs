4f4af807dde43e3c40ec70aeec6486888a7d7cc7
Örs Csák <ors.csak@stud.uni-corvinus.hu>
2021-10-29 10:15:30 +0200

    class

---
+++ week6/Entities/RateData.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+/// <summary>
+/// Summary description for Class1
+/// </summary>
+public class Class1
+{
+	public Class1()
+	{
+		public DateTime Date { get; set; }
+		public string Currency { get; set; }
+		public decimal Value { get; set; }
+	}
+}
